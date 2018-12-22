2b58595ad73dabaf249e02ab1284258ec76d8c25
wotsushi <wotsushi@gmail.com>
2018-12-22 19:53:33 +0900

    Install Onsen UI

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
+import 'onsenui/css/onsenui.css'
+import 'onsenui/css/onsen-css-components.css'
+
 import Vue from 'vue'
+import VueOnsen from 'vue-onsenui'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+
+Vue.use(VueOnsen)
 
 new Vue({
   router,
